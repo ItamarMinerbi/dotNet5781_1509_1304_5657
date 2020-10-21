978f014d50d0a830a659577405c1c1bb0bdb713d
elie arazi <elghiarazi@gmail.com>
2020-10-21 18:36:18 +0300

    Initial code + Welcome1304

---
+++ dotNet5781_00_1509_1304/Program1304.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_1509_1304
+{
+    partial class Program
+    {
+        static partial void Welcome1304()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
--- dotNet5781_00_1509_1304/Program1509.cs
+++ dotNet5781_00_1509_1304/Program1509.cs
@@ -15,16 +15,13 @@ namespace dotNet5781_00_1509_1304
             Console.ReadKey();
         }
 
+        static partial void Welcome1304();
+
         private static void Welcome1509()
         {
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine($"Hello {name}!");
-        }
-
-        private static void Welcome1304() 
-        {
-
         }
     }
 }
