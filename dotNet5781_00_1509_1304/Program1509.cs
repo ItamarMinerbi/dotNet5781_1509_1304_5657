5953354e41195d8a2c6cd882e8b45219fd8ceeab
Itamar Minerbi <iminerbi@g.jct.ac.il>
2020-10-21 13:42:14 +0300

    Initial code + Welcome1509

---
+++ dotNet5781_00_1509_1304/Program1509.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_1509_1304
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1509();
+            Welcome1304();
+            Console.ReadKey();
+        }
+
+        private static void Welcome1509()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+        }
+
+        private static void Welcome1304() 
+        {
+
+        }
+    }
+}
